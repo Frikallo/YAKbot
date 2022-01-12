c44d22287d67829afc6740e2f21c6feef0746507
Frikallo <noahskay@icloud.com>
2022-01-11 20:10:04 -0800

    --

--- Bot/bot.py
+++ Bot/bot.py
@@ -228,7 +228,7 @@ def success_embed(title, description):
 
 
 class dotdict(dict):
-    """ dot.notation access to dictionary attributes """
+    """dot.notation access to dictionary attributes"""
 
     __getattr__ = dict.get
     __setattr__ = dict.__setitem__
