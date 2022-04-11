9d6efeabbed41c79217b3024bcd5f6ea02fa4aa5
Frikallo <noahskay@icloud.com>
2022-04-11 00:03:27 -0700

    --

--- Bot/bot.py
+++ Bot/bot.py
@@ -120,6 +120,31 @@ from subprocess import Popen, PIPE
 import re
 from packaging import version
 
+from pydoc import describe
+import torch
+from omegaconf import OmegaConf
+import sys
+
+sys.path.append(".")
+sys.path.append("././taming_transformers")
+sys.path.append("././latent-diffusion")
+from taming.models import vqgan
+from ldm.util import instantiate_from_config
+
+# @title Import stuff
+import argparse, os, sys, glob
+import numpy as np
+from PIL import Image
+from einops import rearrange
+from torchvision.utils import make_grid
+import transformers
+import gc
+from ldm.util import instantiate_from_config
+from ldm.models.diffusion.ddim import DDIMSampler
+from ldm.models.diffusion.plms import PLMSSampler
+from open_clip import tokenizer
+import open_clip
+
 # Supress warnings
 import warnings
 
@@ -156,6 +181,7 @@ lowerBoundNote = 21
 resolution = 0.25
 discord_white = discord.Color.from_rgb(255, 255, 255)
 MAIN_COLOR = discord_white  # light blue kinda
+available_gpus = 1
 
 use_gpu = False
 save_prefix = "saved"
@@ -219,6 +245,78 @@ known_face_names = [
 ]
 
 # Helper functions
+def load_model_from_config(config, ckpt, verbose=False):
+    print(f"Loading model from {ckpt}")
+    pl_sd = torch.load(ckpt, map_location="cpu")
+    sd = pl_sd["state_dict"]
+    model = instantiate_from_config(config.model)
+    m, u = model.load_state_dict(sd, strict=False)
+    if len(m) > 0 and verbose:
+        print("missing keys:")
+        print(m)
+    if len(u) > 0 and verbose:
+        print("unexpected keys:")
+        print(u)
+
+    model = model.half().cuda()
+    model.eval()
+    return model
+
+
+def load_safety_model(clip_model):
+    """load the safety model"""
+    import autokeras as ak  # pylint: disable=import-outside-toplevel
+    from tensorflow.keras.models import (
+        load_model,
+    )  # pylint: disable=import-outside-toplevel
+    from os.path import expanduser  # pylint: disable=import-outside-toplevel
+
+    home = expanduser("~")
+
+    cache_folder = home + "/.cache/clip_retrieval/" + clip_model.replace("/", "_")
+    if clip_model == "ViT-L/14":
+        model_dir = cache_folder + "/clip_autokeras_binary_nsfw"
+        dim = 768
+    elif clip_model == "ViT-B/32":
+        model_dir = cache_folder + "/clip_autokeras_nsfw_b32"
+        dim = 512
+    else:
+        raise ValueError("Unknown clip model")
+    if not os.path.exists(model_dir):
+        os.makedirs(cache_folder, exist_ok=True)
+
+        from urllib.request import (
+            urlretrieve,
+        )  # pylint: disable=import-outside-toplevel
+
+        path_to_zip_file = cache_folder + "/clip_autokeras_binary_nsfw.zip"
+        if clip_model == "ViT-L/14":
+            url_model = "https://raw.githubusercontent.com/LAION-AI/CLIP-based-NSFW-Detector/main/clip_autokeras_binary_nsfw.zip"
+        elif clip_model == "ViT-B/32":
+            url_model = "https://raw.githubusercontent.com/LAION-AI/CLIP-based-NSFW-Detector/main/clip_autokeras_nsfw_b32.zip"
+        else:
+            raise ValueError("Unknown model {}".format(clip_model))
+        urlretrieve(url_model, path_to_zip_file)
+        import zipfile  # pylint: disable=import-outside-toplevel
+
+        with zipfile.ZipFile(path_to_zip_file, "r") as zip_ref:
+            zip_ref.extractall(cache_folder)
+
+    loaded_model = load_model(model_dir, custom_objects=ak.CUSTOM_OBJECTS)
+    loaded_model.predict(
+        np.random.rand(10 ** 3, dim).astype("float32"), batch_size=10 ** 3
+    )
+
+    return loaded_model
+
+
+def is_unsafe(safety_model, embeddings, threshold=0.5):
+    """find unsafe embeddings"""
+    nsfw_values = safety_model.predict(embeddings, batch_size=embeddings.shape[0])
+    x = np.array([e[0] for e in nsfw_values])
+    return True if x > threshold else False
+
+
 def show(imgs):
     if not isinstance(imgs, list):
         imgs = [imgs]
@@ -343,6 +441,101 @@ def hms(seconds):
     return "{:02d}:{:02d}:{:02d}".format(h, m, s)
 
 
+def run(opt):
+
+    if opt.plms:
+        opt.ddim_eta = 0
+        sampler = PLMSSampler(model)
+    else:
+        sampler = DDIMSampler(model)
+
+    os.makedirs(opt.outdir, exist_ok=True)
+    outpath = opt.outdir
+
+    prompt = opt.prompt
+
+    sample_path = os.path.join(outpath, "samples")
+    os.makedirs(sample_path, exist_ok=True)
+    base_count = len(os.listdir(sample_path))
+
+    all_samples = list()
+    all_samples_images = list()
+    with torch.no_grad():
+        with torch.cuda.amp.autocast():
+            with model.ema_scope():
+                uc = None
+                if opt.scale > 0:
+                    uc = model.get_learned_conditioning(opt.n_samples * [""])
+                for n in range(opt.n_iter):
+                    c = model.get_learned_conditioning(opt.n_samples * [prompt])
+                    shape = [4, opt.H // 8, opt.W // 8]
+                    samples_ddim, _ = sampler.sample(
+                        S=opt.ddim_steps,
+                        conditioning=c,
+                        batch_size=opt.n_samples,
+                        shape=shape,
+                        verbose=False,
+                        unconditional_guidance_scale=opt.scale,
+                        unconditional_conditioning=uc,
+                        eta=opt.ddim_eta,
+                    )
+
+                    x_samples_ddim = model.decode_first_stage(samples_ddim)
+                    x_samples_ddim = torch.clamp(
+                        (x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0
+                    )
+
+                    for x_sample in x_samples_ddim:
+                        torch.cuda.empty_cache()
+                        gc.collect()
+                        x_sample = 255.0 * rearrange(
+                            x_sample.cpu().numpy(), "c h w -> h w c"
+                        )
+                        image_vector = Image.fromarray(x_sample.astype(np.uint8))
+                        image_preprocess = preprocess(image_vector).unsqueeze(0)
+                        with torch.no_grad():
+                            image_features = clip_model.encode_image(image_preprocess)
+                        image_features /= image_features.norm(dim=-1, keepdim=True)
+                        query = image_features.cpu().detach().numpy().astype("float32")
+                        unsafe = is_unsafe(safety_model, query, 0.5)
+                        if not unsafe:
+                            all_samples_images.append(image_vector)
+                        else:
+                            return (
+                                None,
+                                None,
+                                "Sorry, potential NSFW content was detected on your outputs by our NSFW detection model. Try again with different prompts. If you feel your prompt was not supposed to give NSFW outputs, this may be due to a bias in the model. Read more about biases in the Biases Acknowledgment section below.",
+                            )
+                        # Image.fromarray(x_sample.astype(np.uint8)).save(os.path.join(sample_path, f"{base_count:04}.png"))
+                        base_count += 1
+                    all_samples.append(x_samples_ddim)
+
+    # additionally, save as grid
+    grid = torch.stack(all_samples, 0)
+    grid = rearrange(grid, "n b c h w -> (n b) c h w")
+    grid = make_grid(grid, nrow=opt.n_samples)
+    # to image
+    grid = 255.0 * rearrange(grid, "c h w -> h w c").cpu().numpy()
+
+    Image.fromarray(grid.astype(np.uint8)).save(os.path.join(outpath, f"out.png"))
+    return (Image.fromarray(grid.astype(np.uint8)), all_samples_images, None)
+
+
+answer = input("Load Latent Diffusion Models? (y/n) ")
+if answer == "y":
+    config = OmegaConf.load(
+        "latent-diffusion/configs/latent-diffusion/txt2img-1p4B-eval.yaml"
+    )
+    model = load_model_from_config(config, f"model/txt2img_f8_large.ckpt")
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    model = model.to(device)
+    # NSFW CLIP Filter
+    safety_model = load_safety_model("ViT-B/32")
+    clip_model, _, preprocess = open_clip.create_model_and_transforms(
+        "ViT-B-32", pretrained="openai"
+    )
+else:
+    print("Suppressing Latent Diffusion")
 # Settings
 args = argparse.Namespace(
     config="./checkpoints/12xdqrwd-config",
@@ -447,9 +640,7 @@ async def on_ready():
     print(f"Elapsed Startup Time: {endtime}")
     date = strftime("%a, %d %b %Y %H:%M:%S", localtime())
     print(date)
-    webhook = DiscordWebhook(
-        url=os.environ["webhook"],
-    )
+    webhook = DiscordWebhook(url=os.environ["webhook"],)
     output = f"""We have logged in as YAKbot#7261
 Connected to: {len(bot.guilds)} guilds
 Connected to: {len(bot.commands)} commands
@@ -1150,7 +1341,7 @@ async def imagine(ctx):
             if not torch.cuda.is_available():
                 default_image_size = 412  # no GPU found
             elif (
-                get_device_properties(0).total_memory <= 2**33
+                get_device_properties(0).total_memory <= 2 ** 33
             ):  # 2 ** 33 = 8,589,934,592 bytes = 8 GB
                 default_image_size = 318  # <8GB VRAM
 
@@ -2137,7 +2328,7 @@ async def help(ctx):
     async with ctx.channel.typing():
         embed = discord.Embed(
             title="YAKbot Help",
-            description=f"`.rembg [Attached Image]`\n**removes background from attatched image**\n\n`.esrgan [Attatchment]`\n**YAKbot will use a pretrained ESRGAN upscaler to upscale you images resolution by up to 4 times**\n\n`.status`\n**sends embed message with all relevent device stats for YAKbot**\n\n`.imagine [Prompt]`\n**uses CLIP+VQGAN open generation to create an original image from your prompt**\n\n`.facehq, .wikiart, .default, .d1024`\n**Changes YAKbots VQGAN+CLIP model to one trained solely on faces, art or default configuration**\n\n`.square, .landscape, .portrait`\n**YAKbot will update his size configurations for generations to your specified orientation**\n\n`.seed [Desired Seed]`\n**Changes YAKbots seed for all open generation (if 0 will set to random)**\n\n`.faces [Attatchment]`\n**YAKbot will look through your photo and try to find any recognizable faces**\n\n`.colorize [Attatchment]`\n**YAKbot will turn your black and white attatchment into a colorized version**\n\n`.outline [Prompt]`\n**YAKbot will contact a local GPT3 model that will synthasize and look for essays on your prompt while outputting an outline/list of ideas/facts about your prompt to help kickstart your projects**\n\n__Any Attatchments Sent In This Channel Will Be Identified And Captioned By YAKbot (To Prevent Captioning Include --nc In Your Message)__",
+            description=f"`.rembg [Attached Image]`\n**removes background from attatched image**\n\n`.esrgan [Attatchment]`\n**YAKbot will use a pretrained ESRGAN upscaler to upscale you images resolution by up to 4 times**\n\n`.status`\n**sends embed message with all relevent device stats for YAKbot**\n\n`.imagine [Prompt]`\n**uses CLIP+VQGAN open generation to create an original image from your prompt**\n\n`.facehq, .wikiart, .default, .d1024`\n**Changes YAKbots VQGAN+CLIP model to one trained solely on faces, art or default configuration**\n\n`.square, .landscape, .portrait`\n**YAKbot will update his size configurations for generations to your specified orientation**\n\n`.seed [Desired Seed]`\n**Changes YAKbots seed for all open generation (if 0 will set to random)**\n\n`.faces [Attatchment]`\n**YAKbot will look through your photo and try to find any recognizable faces**\n\n`.colorize [Attatchment]`\n**YAKbot will turn your black and white attatchment into a colorized version**\n\n`.latentdiffusion [Prompt]`\n**This command is another Text2Image method like `.imaging` but uses a method called latent diffusion. innitiate this method by using the command.**\n\n`.outline [Prompt]`\n**YAKbot will contact a local GPT3 model that will synthasize and look for essays on your prompt while outputting an outline/list of ideas/facts about your prompt to help kickstart your projects**\n\n__Any Attatchments Sent In This Channel Will Be Identified And Captioned By YAKbot (To Prevent Captioning Include --nc In Your Message)__",
             color=discord_white,
         )
         await ctx.channel.send(embed=embed)
@@ -2330,6 +2521,69 @@ async def outline(ctx):
         await ctx.channel.send(embed=embed)
 
 
+@bot.command()
+async def latentdiffusion(ctx):
+    available_gpus = 0
+    if ctx.channel.id != channel_id:
+        return
+    print("Command Loaded")
+    async with ctx.channel.typing():
+        input = ctx.message.content
+        input = str(input[17 : len(input)])
+        Prompt = input
+        Steps = 50
+        ETA = 0
+        Iterations = 3
+        Width = 256
+        Height = 256
+        Samples_in_parallel = 3
+        Diversity_scale = 5.0
+        PLMS_sampling = True
+        args = argparse.Namespace(
+            prompt=Prompt,
+            outdir=f"out/",
+            ddim_steps=Steps,
+            ddim_eta=ETA,
+            n_iter=Iterations,
+            W=Width,
+            H=Height,
+            n_samples=Samples_in_parallel,
+            scale=Diversity_scale,
+            plms=PLMS_sampling,
+            nsfw_threshold=0.5,
+        )
+        im_mes = await ctx.channel.send("ㅤㅤ")
+        message = await ctx.channel.send(
+            f"_{input}_\n`Processing...`\n```python\nddim_steps={args.ddim_steps}\nddim_eta={args.ddim_eta}\nn_iter={args.n_iter}\nn_samples={args.n_samples}\nwidth={args.W}\nheight={args.H}\nscale={args.scale}\nplms={args.plms}\n```"
+        )
+        start = time.time()
+        try:
+            run(args)
+        except Exception as e:
+            print(e)
+            print("Error")
+            embed = discord.Embed(
+                title=f"Generation error",
+                description=f"{e}",
+                color=discord.Color.red(),
+            )
+            await ctx.send(embed=embed)
+        end = time.time() - start
+        end = int(end)
+        channel2 = bot.get_channel(955787994286129172)
+        image = await channel2.send(file=discord.File("./out/out.png"))
+        image = image.attachments[0].url
+        await message.edit(
+            content=f"_{input}_\n`Uploading...`\n```python\nddim_steps={args.ddim_steps}\nddim_eta={args.ddim_eta}\nn_iter={args.n_iter}\nn_samples={args.n_samples}\nwidth={args.W}\nheight={args.H}\nscale={args.scale}\nplms={args.plms}\n```"
+        )
+        time.sleep(1)
+        await im_mes.edit(content=image)
+        await message.edit(
+            content=f"_{input}_\n`Completed in {end} seconds.`\n```python\nddim_steps={args.ddim_steps}\nddim_eta={args.ddim_eta}\nn_iter={args.n_iter}\nn_samples={args.n_samples}\nwidth={args.W}\nheight={args.H}\nscale={args.scale}\nplms={args.plms}\n```"
+        )
+    available_gpus = 1
+
+
 @bot.event
 async def on_command_error(ctx, error):
     # if command has local error handler, return
@@ -2455,6 +2709,8 @@ async def on_command_error(ctx, error):
 
 @bot.event
 async def on_command(ctx):
+    if available_gpus == 0:
+        await ctx.reply("Im Busy!")
     api.command_run(ctx)
 
 
--- Bot/classify.py
+++ Bot/classify.py
@@ -50,10 +50,7 @@ def load(categorylist):
             if i not in categories:
                 categories.append(i)
     elif load_categories == "emojis":
-        categories = open(
-            "./categories/emojis.txt",
-            encoding="utf8",
-        ).readlines()
+        categories = open("./categories/emojis.txt", encoding="utf8",).readlines()
     c_encs = [
         perceptor.encode_text(clip.tokenize(category).cuda()).detach().clone()
         for category in categories
